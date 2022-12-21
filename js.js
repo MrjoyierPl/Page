9e79a576ba206009392d2af3f755ce5bf1a3d364
Mikołaj Jasiński <115838165+MrjoyierPl@users.noreply.github.com>
2022-12-21 21:33:30 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome){
+    $('#iframeAudio').remove()
+}
+else {
+    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+}
+
+function playMusic(){
+    var music = new Audio('weird.mp3');
+    music.play();
+    }
